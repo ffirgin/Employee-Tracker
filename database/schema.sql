d0f2864e67848ca3fca2a190a1a5354767635238
Griffin <ffirgn@gmail.com>
2022-12-16 15:27:04 -0800

    remove not null from manager id

--- database/schema.sql
+++ database/schema.sql
@@ -23,6 +23,6 @@ CREATE TABLE employee (
   last_name VARCHAR(50) NOT NULL,
   role_id INT NOT NULL,
   FOREIGN KEY (role_id) REFERENCES role(id),
-  manager_id INT NOT NULL,
+  manager_id INT,
   FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
